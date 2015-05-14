d8d7b281206db27130744010bd55b5c67dee30df
Steven Sherrie <steve@wasteofpaper.com>
2015-05-14 13:03:24 -0400

    Add checkbox directive

---
+++ src/elements/checkbox/checkbox.js
@@ -0,0 +1,59 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('semantic.ui.elements.checkbox', [])
+
+    .directive('smCheckbox', smCheckbox);
+
+  function smCheckbox() {
+    return {
+      restrict: 'E',
+      require: '?ngModel',
+      transclude: true,
+      scope: {
+        ngDisabled: '='
+      },
+      template: '<div class="ui checkbox" ng-class="{ checked: isChecked }">' +
+                  '<input type="checkbox" ng-disabled="ngDisabled" ng-checked="isChecked">' +
+                  '<label ng-transclude></label>' +
+                '</div>',
+
+      link: function(scope, element, attrs, ngModel) {
+
+
+        element.bind('click', toggleFn);
+
+        if (ngModel) {
+          ngModel.$render = function() { 
+            scope.isChecked = ngModel.$viewValue;
+          };
+        }
+
+        if (attrs.toggle !== void 0) {
+          angular.element(element[0].firstChild).addClass('toggle');
+        }
+        else if (attrs.slider !== void 0) {
+          angular.element(element[0].firstChild).addClass('slider');
+        }
+
+        if (attrs.ariaLabel === void 0) {
+          element.attr('aria-label', element[0].textContent.trim());
+        }
+
+        function toggleFn() {
+
+          if (scope.ngDisabled) { return; }
+
+          scope.isChecked = !scope.isChecked;
+          if (ngModel) {
+            ngModel.$setViewValue(scope.isChecked);
+          }
+          scope.$apply();
+        }
+
+      }
+    };
+
+  }
+})();
