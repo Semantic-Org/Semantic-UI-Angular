7a1f2f7042f4a0de5d162952f5cee437977a1e70
Gautham Ramachandran <gautham@evernym.us>
2015-05-24 22:27:52 +0530

    Loading indicator for button

--- src/elements/button/demo/buttons.module.js
+++ src/elements/button/demo/buttons.module.js
@@ -2,9 +2,15 @@
     'use strict';
 
     angular
-        .module('semantic.ui.angular.buttons',[
-            'ngAria',
-            'semantic.ui.elements.button'
-        ]);
+      .module('semantic.ui.angular.buttons',[
+        'ngAria',
+        'semantic.ui.elements.button'
+      ])
+      .controller('ButtonsCtrl', function($scope) {
+        $scope.isLoading = false;
+        $scope.toggleLoader = function() {
+          $scope.isLoading = !$scope.isLoading;
+        }
+      });
 
 })();
