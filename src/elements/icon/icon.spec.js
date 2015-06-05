849be445b85eb20f893e0a249885a85064d3051e
Gautham Ramachandran <gautham@evernym.us>
2015-06-05 10:13:07 +0530

    Removed the icon class from tests

--- src/elements/icon/icon.spec.js
+++ src/elements/icon/icon.spec.js
@@ -11,14 +11,14 @@ describe('Semantic-UI: Elements - smIcon', function() {
   }));
 
   it('has to be font icon with classes user & icon', function() {
-    var smIcon = $compile('<sm-icon sm-font-icon="user icon"></sm-icon>')($scope);
+    var smIcon = $compile('<sm-icon sm-font-icon="user"></sm-icon>')($scope);
     $scope.$digest();
     expect(smIcon.hasClass('user')).toBe(true);
   });
 
 
   it('has to be font icon with custom size', function() {
-    var smIcon = $compile('<sm-icon sm-font-icon="user icon" sm-icon-size="60px"></sm-icon>')($scope);
+    var smIcon = $compile('<sm-icon sm-font-icon="user" sm-icon-size="60px"></sm-icon>')($scope);
     $scope.$digest();
     expect(smIcon.css('font-size')).toBe("60px");
   })
