b6ca17ec00a5c6be46848e1d5fd869bc7a8b1d28
Patrick Porto <patrick.s.porto@gmail.com>
2015-08-28 20:21:08 -0300

    Add loading directive

---
+++ src/behaviors/loading/loading.js
@@ -0,0 +1,28 @@
+(function() {
+'use strict';
+
+angular
+  .module('semantic.ui.behaviors.loading', [])
+
+  .directive('smLoading', smLoading);
+
+  smLoading.$inject = ['$parse'];
+
+  function smLoading($parse) {
+    return {
+      restrict:'A',
+      link: function(scope, element, attrs, ctrl, transclude) {
+        scope.$watch(function() {
+          return $parse(attrs.smLoading)(scope);
+        }, function(loading) {
+          if (loading) {
+            element.addClass('loading');
+          } else {
+            element.removeClass('loading');
+          }
+        });
+      }
+    };
+  }
+
+})();
